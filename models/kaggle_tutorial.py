0895c0c8bb110cbe96871d2eae2fde9250fae049
jayman386 <jmoriarty@gatech.edu>
2016-09-13 01:25:33 -0400

    Adding Kaggle tutorial

---
+++ models/kaggle_tutorial.py
@@ -0,0 +1,17 @@
+import csv as csv 
+import numpy as np
+
+training_file_object = csv.reader(open('../data/train.csv')) 
+test_file_object = csv.reader(open('../data/test.csv')) 
+
+training_header = next(training_file_object)
+training_data = list(training_file_object)
+
+test_header = next(test_file_object)
+test_data = list(test_file_object)
+
+training_data = np.array(training_data)       
+
+number_passengers = np.size(training_data[0::,1].astype(np.float))
+number_survived = np.sum(training_data[0::,1].astype(np.float))
+proportion_survivors = number_survived / number_passengers
