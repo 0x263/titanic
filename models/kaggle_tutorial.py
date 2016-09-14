8cad1658507dddc474c4d461ac2bb47628f433d3
jayman386 <jmoriarty@gatech.edu>
2016-09-14 00:37:03 -0400

    update

--- models/kaggle_tutorial.py
+++ models/kaggle_tutorial.py
@@ -1,10 +1,10 @@
-import csv as csv 
+import csv as csv
 import matplotlib as plt
 import numpy as np
 import pandas as pd
 
-training_file_object = csv.reader(open('../data/train.csv')) 
-test_file_object = csv.reader(open('../data/test.csv')) 
+training_file_object = csv.reader(open('../data/train.csv'))
+test_file_object = csv.reader(open('../data/test.csv'))
 
 training_header = next(training_file_object)
 training_data = list(training_file_object)
@@ -12,7 +12,7 @@ training_data = list(training_file_object)
 test_header = next(test_file_object)
 test_data = list(test_file_object)
 
-training_data = np.array(training_data)       
+training_data = np.array(training_data)
 
 number_passengers = np.size(training_data[0::,1].astype(np.float))
 number_survived = np.sum(training_data[0::,1].astype(np.float))
